318b0ac1b2daf0816edf831baa3595f8afd275cf
Hoang Nguyen <ngviethoagnnn@gmail.com>
2025-08-17 13:44:40 +0700

    hello

---
+++ Hello.go
@@ -0,0 +1,4 @@
+func Introduction() {
+    fmt.Println("Welcome to my Github!")
+    fmt.Println("Hope you have a good day, 1337")
+}
--- Hello.rs
+++
@@ -1,4 +0,0 @@
-fn Introduction() {
-    println!("Welcome to my Github!");
-    println!("Hope you have a good day, 1337");
-}
